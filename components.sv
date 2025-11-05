d1ae49c05e5eb3c0a910a0559d541500c6a8c1f0
rafamxv <rmxvmail@gmail.com>
2025-11-05 17:18:33 -0300

    adição do banco de registradores

--- components.sv
+++ components.sv
@@ -65,3 +65,37 @@ always_comb begin
     endcase
 end
 endmodule
+
+
+module regfile(
+    input clk,
+    input rst,
+    input we,
+    input [4:0] rs1,
+    input [4:0] rs2,
+    input [4:0] disp,
+    input [4:0] rd,
+    input [31:0] wd,
+    output [31:0] rd1,
+    output [31:0] rd2,
+    output [31:0] rdisp
+);
+
+    reg [31:0] regs [31:0];
+	 
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            integer i;
+            for (i = 0; i < 32; i = i + 1)
+                regs[i] <= 32'b0;
+            regs[2] <= 32'h100103FC;
+        end 
+        else if (we && rd != 0)
+            regs[rd] <= wd;
+    end
+	 
+    assign rd1 = (rs1 == 0) ? 32'b0 : regs[rs1];
+    assign rd2 = (rs2 == 0) ? 32'b0 : regs[rs2];
+    assign rdisp = (disp == 0) ? 32'b0 : regs[disp];
+
+endmodule
